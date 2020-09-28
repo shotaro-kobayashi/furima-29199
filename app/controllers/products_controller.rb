ecc1a428727b0e28253cd36d138cdcb140c285d3
shotaro-kobayashi <shotaro.kobayashi.9x@stu.hosei.ac.jp>
2020-09-28 21:57:42 +0900

    before_action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create]
+  before_action :set_product, only: [:edit, :show,:update]
   def index
     @products = Product.all
   end
@@ -18,23 +19,18 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-   @product = Product.find(params[:id])
-    
     if @product.update(product_params)
       redirect_to product_path(@product.id)
     else
       redirect_to action: :edit
     end
-    
-    end
+  end
   
 
 
@@ -44,5 +40,8 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:product_name, :text, :category_id,:area_id,:day_id,:status_id,:delivery_cost_id,:price,:image).merge(user_id: current_user.id)
   end
   
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
 end
